95586407bdc86414c9e2dd41a6aaec6ee8e869ca
yjgaia <yjdev88@gmail.com>
2024-09-04 13:29:56 +0900

    remove token manager

--- src/token/TokenManager.ts
+++
@@ -1,7 +0,0 @@
-import EventContainer from "../event/EventContainer.js";
-
-export default abstract class TokenManager extends EventContainer<{
-  tokenChanged: (token: string | undefined) => void;
-}> {
-  public abstract get token(): string | undefined;
-}
