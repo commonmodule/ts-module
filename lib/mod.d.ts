f8c60e6fd07a69820e1686a2babe6ba9d2590b85
yjgaia <yjdev88@gmail.com>
2024-09-30 17:04:35 +0900

    Refactor file extensions to use TypeScript in mod.d.ts and mod.ts

--- lib/mod.d.ts
+++ lib/mod.d.ts
@@ -1,9 +1,9 @@
-export type { default as EventContainer } from "./event/EventContainer.js";
-export type { default as EventTreeNode } from "./tree/EventTreeNode.js";
-export type { default as TreeNode } from "./tree/TreeNode.js";
-export type { default as KebabCase } from "./types/KebabCase.js";
-export type { default as ArrayUtils } from "./utils/ArrayUtils.js";
-export type { default as IntegerUtils } from "./utils/IntegerUtils.js";
-export type { default as JsonUtils, JsonValue } from "./utils/JsonUtils.js";
-export type { default as StringUtils } from "./utils/StringUtils.js";
+export type { default as EventContainer } from "./event/EventContainer.ts";
+export type { default as EventTreeNode } from "./tree/EventTreeNode.ts";
+export type { default as TreeNode } from "./tree/TreeNode.ts";
+export type { default as KebabCase } from "./types/KebabCase.ts";
+export type { default as ArrayUtils } from "./utils/ArrayUtils.ts";
+export type { default as IntegerUtils } from "./utils/IntegerUtils.ts";
+export type { default as JsonUtils, JsonValue } from "./utils/JsonUtils.ts";
+export type { default as StringUtils } from "./utils/StringUtils.ts";
 //# sourceMappingURL=mod.d.ts.map
