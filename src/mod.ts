257ecaa9a7fb3b3b17041f5bfdb7060d33ca9896
yjgaia <yjdev88@gmail.com>
2024-11-09 14:19:56 +0900

    Export ArrayUtils as a named export in mod.ts

--- src/mod.ts
+++ src/mod.ts
@@ -2,7 +2,7 @@ export type { default as EventContainer } from "./event/EventContainer.ts";
 export type { default as EventTreeNode } from "./tree/EventTreeNode.ts";
 export type { default as TreeNode } from "./tree/TreeNode.ts";
 export type { default as KebabCase } from "./types/KebabCase.ts";
-export type { default as ArrayUtils } from "./utils/ArrayUtils.ts";
+export { default as ArrayUtils } from "./utils/ArrayUtils.ts";
 export type { default as Debouncer } from "./utils/Debouncer.ts";
 export type { default as IntegerUtils } from "./utils/IntegerUtils.ts";
 export type { default as JsonUtils, JsonValue } from "./utils/JsonUtils.ts";
