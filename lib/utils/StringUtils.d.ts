789719962b72978e9a11881355cd7e9493cc94bb
yjgaia <yjdev88@gmail.com>
2024-11-11 15:06:34 +0900

    Add formatNumberWithCommas method to StringUtils class

--- lib/utils/StringUtils.d.ts
+++ lib/utils/StringUtils.d.ts
@@ -1,6 +1,7 @@
 declare class StringUtils {
     capitalize(input: string): string;
     isKebabCase(str: string): boolean;
+    formatNumberWithCommas(value: string, decimals?: number): string;
 }
 declare const _default: StringUtils;
 export default _default;
